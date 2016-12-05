5ea995b6d9b254021bbdcda00d0facbd8e3190c0
Trung Duc <ducnt.ts24@gmail.com>
2016-12-05 13:46:36 +0700

    cau truc lai models

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,8 +6,8 @@ use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
-use App\danhsachkynangchuyenmon;
-use App\danhsachsothich;
+use App\Models\danhsachkynangchuyenmon;
+use App\Models\danhsachsothich;
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
